45e8fd9b692f39c0a5eb7cec2cbdcba0e12c1f8c
sixpidev <yupipi93@gmail.com>
2017-11-22 14:15:21 +0100

    añadido boton para volver al manu

---
+++ Unity Project/Assets/NUESTRO/Scripts Nuestros/Scripts Menu de Seleccion/BackMenu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class BackMenu : MonoBehaviour, IPointerClickHandler
+{
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		SceneManager.LoadScene("menu");
+	}
+}
